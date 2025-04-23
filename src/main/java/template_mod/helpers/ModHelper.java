94349ee1810d10eb20a422d39e0a3a6a85419e89
kakami <kakamisound@outlook.com>
2025-04-23 22:26:39 +0800

    添加了能力的模板

--- src/main/java/template_mod/helpers/ModHelper.java
+++ src/main/java/template_mod/helpers/ModHelper.java
@@ -15,6 +15,7 @@ public class ModHelper {
     public static final String mod_id = "{{{mod_id}}}_mod";
     public static final String res_id = "{{{mod_id}}}_res";
 
+    // 各种卡 能力等等的ID
     public static String makeID(Class<?> cla) {
         return makeID(cla.getName());
     }
@@ -23,6 +24,7 @@ public class ModHelper {
         return mod_id + ":" + name;
     }
 
+    // 卡牌图片路径
     public static String makeCardImgPath(AbstractCard.CardType type, Class<?> cla) {
         String type_name;
         switch (type) {
@@ -44,20 +46,36 @@ public class ModHelper {
         return res_id + "/img/cards/" + type_name + "/" + name + ".png";
     }
 
+    // 遗物图片路径
     public static String makeRelicImgPath(Class<?> cla) {
         String name = cla != null ? cla.getName() : "Beta";
         return res_id + "/img/relics/" + name + ".png";
     }
 
+    // 能力大图路径 STSPicture 提供的是84x84
+    public static String makePowerImgPath84(Class<?> cla) {
+        String name = cla != null ? cla.getName() : "Beta";
+        return res_id + "/img/powers/" + name + "84.png";
+    }
+
+    // 能力小图路径 STSPicture 提供的是32x32
+    public static String makePowerImgPath32(Class<?> cla) {
+        String name = cla != null ? cla.getName() : "Beta";
+        return res_id + "/img/powers/" + name + "32.png";
+    }
+
+    // 日志
     public static void log(Object obj, String str) {
         Logger logger = LogManager.getLogger(obj.getClass());
         logger.info(str);
     }
 
+    // 使用卡牌随机数随机
     public static int cardRand(int size) {
         return AbstractDungeon.cardRng.random(size - 1);
     }
 
+    // 获取存活怪物
     public static ArrayList<AbstractMonster> getAliveMonsters() {
         ArrayList<AbstractMonster> ret_list = new ArrayList<>();
         for (AbstractMonster m : AbstractDungeon.getMonsters().monsters) {
@@ -68,7 +86,7 @@ public class ModHelper {
         return ret_list;
     }
 
-    // 获取除了指定怪物以外的怪物
+    // 获取除了指定怪物以外的存活怪物
     public static ArrayList<AbstractMonster> getAliveMonsters(AbstractMonster except) {
         ArrayList<AbstractMonster> ret_list = new ArrayList<>();
         for (AbstractMonster m : AbstractDungeon.getMonsters().monsters) {
@@ -82,6 +100,7 @@ public class ModHelper {
     public interface Lambda extends Runnable {
     }
 
+    // 匿名添加action
     public static void addToBotAbstract(Lambda func) {
         AbstractDungeon.actionManager.addToBottom(new AbstractGameAction() {
             @Override
@@ -92,6 +111,7 @@ public class ModHelper {
         });
     }
 
+    // 匿名添加action
     public static void addToTopAbstract(Lambda func) {
         AbstractDungeon.actionManager.addToTop(new AbstractGameAction() {
             @Override
---
+++ src/main/java/template_mod/powers/TemplatePower.java
@@ -0,0 +1,54 @@
+package {{{mod_id}}}_mod.powers;
+
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import {{{mod_id}}}_mod.helpers.ModHelper;
+
+public class {{{character}}}Power extends AbstractPower {
+
+    // 一般不改
+    private static final boolean BETA = false;  // 使用测试卡图
+
+    // 不用改
+    public static final String POWER_ID;
+    private static final PowerStrings powerStrings;
+    public static final String NAME;
+    public static final String[] DESCRIPTIONS;
+
+    static {
+        POWER_ID = ModHelper.makeID({{{character}}}Power.class);
+        powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+        NAME = powerStrings.NAME;
+        DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+    }
+
+    public {{{character}}}Power(AbstractCreature owner, int Amount) {
+        this.name = NAME;
+        this.ID = POWER_ID;
+        this.owner = owner;
+        this.type = PowerType.BUFF;
+
+        // 如果需要不能叠加的能力，只需将上面的Amount参数删掉，并把下面的Amount改成-1就行
+        this.amount = Amount;
+
+//        // 添加一大一小两张能力图
+//        String path128 = ModHelper.makePowerImgPath84(BETA ? null : {{{character}}}Power.class);
+//        String path48 = ModHelper.makePowerImgPath32(BETA ? null : {{{character}}}Power.class);
+//        this.region128 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage(path128), 0, 0, 84, 84);
+//        this.region48 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage(path48), 0, 0, 32, 32);
+
+        // 使用官方能力图 和上面2选1
+        this.loadRegion("loop");
+
+        // 首次添加能力更新描述
+        this.updateDescription();
+    }
+
+    // 能力在更新时如何修改描述
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0];
+    }
+
+}
